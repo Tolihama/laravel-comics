20704278ea4ff159f2f61bdfd2db09ba50101f93
Tolihama <fra070@gmail.com>
2022-01-24 19:25:36 +0100

    Header e footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,40 @@
-<footer>
+<footer class="main-footer">
+    <div class="container">
+        <div class="signup">
+            <a href="#">Sign-Up Now!</a>
+        </div>
+        <div class="social">
+            <ul>
+                <li>
+                    Follow us
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-facebook.png') }}" alt="Facebook">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-twitter.png') }}" alt="Twitter">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-youtube.png') }}" alt="Youtube">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-pinterest.png') }}" alt="Pinterest">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('images/footer-periscope.png') }}" alt="Periscope">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
     
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,17 @@
-<header>
+<header class="main-header">
+    <div class="container d-flex">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo" class="py-3">
+        <nav class="h100">
+            <ul class="d-flex">
+                <li class="active">
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
 
 </header>
